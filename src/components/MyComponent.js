02253aea18caa12257c737569be9b1522c140bbe
Elena Panina <Anazapta123@mail.ru>
2022-11-26 16:42:16 +0300

    create react

---
+++ src/components/MyComponent.js
@@ -0,0 +1,10 @@
+import './MyComponent.css';
+function MyComponent() {
+    return (
+        <div className="MyComponent">
+            Hello, Functional Component!
+        </div>
+    );
+}
+
+export default MyComponent;
